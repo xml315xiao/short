93a7643186457264ec8d0b8935627096086c3563
YuanLong <xiaomada315@163.com>
2016-08-31 14:54:44 +0800

    www.oo.cc

--- jump.php
+++ jump.php
@@ -6,7 +6,7 @@ $path = parse_url($short_url);
 $short_code = trim($path['path'], '/');
 
 if (stripos($short_code, 'jump') !== FALSE) {
-    header('Location: http://www.oo.cc');
+    header('Location: http://oo.cc');
 } elseif (stripos($short_code, 'create') !== FALSE) {
     header('Location: http://oo.cc/create.php');
 } else {
@@ -15,7 +15,7 @@ if (stripos($short_code, 'jump') !== FALSE) {
     $long_url = $short->parseShortCode($short_code);
 
     if ( FALSE === $long_url ) {
-        header('Location: http://www.oo.cc');
+        header('Location: http://oo.cc');
     } else {
         header('Location: '. $long_url);
     }
